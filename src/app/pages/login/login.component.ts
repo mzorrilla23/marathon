e8feebc0e92ffce2c5fbf78f17e3cc49f5402130
Marcos Zorrilla <marc.zg21@gmail.com>
2021-10-07 18:10:39 -0500

    initial commit

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, AfterViewInit, ElementRef, NgZone, ViewChild, SystemJsNgModuleLoader } from '@angular/core';
+
+
+@Component({
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css']
+  })
+
+  export class LoginComponent implements OnInit, AfterViewInit {
+
+
+    constructor(){
+
+    }
+
+    
+    ngOnInit(): void {
+        throw new Error('Method not implemented.');
+    }
+
+
+
+  }
